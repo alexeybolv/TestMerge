72869f588c3d5dc54ea64027d87da57289fb73d2
Alexey Bolvonovich <alexeybolv@gmail.com>
2019-01-06 19:53:54 +0300

    Initial Commit

---
+++ TestMerge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMerge
+//
+//  Created by ALIAKSEI BALVANOVICH on 1/6/19.
+//  Copyright © 2019 Alexey Bolvonovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
